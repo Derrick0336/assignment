12a4b21e0f9acf7c754126ee03016a32572e6900
Derrick <150381139+Derrick0336@users.noreply.github.com>
2025-01-23 12:27:46 +0300

    assignment 2 merge_list.c

--- assignment 2 merge_list.c
+++ assignment 2 merge_list.c
@@ -1,41 +1,36 @@
 #include <stdio.h>
 #include <stdlib.h>
-
 typedef struct Node {
     int data;
     struct Node* next;
 } Node;
-
 Node* merge(Node* list1, Node* list2) {
     Node dummy, *tail = &dummy;
     dummy.next = NULL;
-
     while (list1 && list2) {
         tail->next = (list1->data <= list2->data) ? list1 : list2;
-        if (list1->data <= list2->data) list1 = list1->next;
+     if (list1->data <= list2->data) list1 = list1->next;
         else list2 = list2->next;
-        tail = tail->next;
+    tail = tail->next;
     }
     tail->next = list1 ? list1 : list2;
     return dummy.next;
 }
-
 void printList(Node* head) {
     while (head) {
-        printf("%d -> ", head->data);
-        head = head->next;
+     printf("%d -> ", head->data);
+     head = head->next;
     }
     printf("NULL\n");
 }
-
 Node* createList(int* arr, int n) {
     Node *head = NULL, *tail = NULL;
     for (int i = 0; i < n; i++) {
-        Node* newNode = (Node*)malloc(sizeof(Node));
-        newNode->data = arr[i];
+    Node* newNode = (Node*)malloc(sizeof(Node));
+ newNode->data = arr[i];
         newNode->next = NULL;
         if (!head) head = tail = newNode;
-        else tail = tail->next = newNode;
+    else tail = tail->next = newNode;
     }
     return head;
 }
